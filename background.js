fcf1e9355f8ec32fe578599e3189b25b0103b2a3
FlorianSchroevers <f.e.schroevers@gmail.com>
2020-02-25 12:44:52 +0100

    added direct indexing

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 var direction = 1;
-var currentIndex = -1;
 
 browser.commands.onCommand.addListener(function(command) {
 	if (command == "next-tab") {
@@ -12,25 +11,17 @@ browser.commands.onCommand.addListener(function(command) {
 
 	// find tab that is currently open
 	var tabQuery = browser.tabs.query({currentWindow: true, active: true});
-	tabQuery.then(findOtherTabs, onError);
+	tabQuery.then(findOtherTab, onError);
 })
 
-function findOtherTabs(tabs) {
-	currentIndex = tabs[0].index;
+function findOtherTab(currentTabs) {
 	// find all other tabs
-	let otherTabs = browser.tabs.query({currentWindow: true});
-	otherTabs.then(switchTab, onError);
+	let otherTab = browser.tabs.query({index: currentTabs[0].index + direction});
+	otherTab.then(switchTab, onError);
 }
 
-function switchTab(otherTabs) {
-	// search for the tab that is 1 in the right direction of
-	// the currently open tab. switch to that tab 
-	for (let t of otherTabs) {
-		if (t.index + direction == currentIndex) {
-			browser.tabs.update(t.id, {active: true});
-			break;
-		}
-	}
+function switchTab(otherTab) {
+	browser.tabs.update(otherTab.id, {active: true});
 }
 
 function onError(msg) {
